1c23fa91e180a3a9f14fdce43cc4ba1e1df12c6f
Meraj Naqui <mailtomeraj@gmail.com>
2024-10-20 21:32:31 +0530

    first commit

---
+++ src/components/DataTable.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-card
+    title="Posts"
+    flat
+  >
+    <template v-slot:text>
+        <v-text-field
+          v-model="search"
+          label="Search"
+          prepend-inner-icon="mdi-magnify"
+          variant="outlined"
+          hide-details
+          single-line
+        ></v-text-field>
+      </template>
+
+  
+    <div v-if="posts">
+        <v-data-table :items="posts"     :search="search"></v-data-table>
+    
+    </div> 
+    </v-card>
+</template>
+
+<script setup>
+import {ref, onMounted} from 'vue'
+import getAllPosts from "@/services/getAllPosts"
+const  search = ref('')
+const {posts, error, load} = getAllPosts()
+
+onMounted(() => {
+  load()
+});
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+<DataTable/>
+</template>
+
+<script setup>
+  //
+</script>
